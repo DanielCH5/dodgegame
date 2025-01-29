704185d311e912736cd13a1d140f40423cb82207
DanielCH5 <danielc.hansen1@gmail.com>
2025-01-29 14:21:29 +0100

    Created border

--- index.php
+++ index.php
@@ -5,15 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dodge Game</title>
     <link rel="stylesheet" href="styles.css">
+
 </head>
 <body>
 
 <div id="game">
+
     <div id="player">
 
     </div>
 </div>
 
-<script src="movement.js"> </script>
+<script src="movement.js"></script>
 </body>
 </html>
