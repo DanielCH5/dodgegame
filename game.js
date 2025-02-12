7cb52400f3b78fa291f6df39769f9375f8343f47
DanielCH5 <danielc.hansen1@gmail.com>
2025-02-12 03:43:15 +0100

    made enemy move once, need to make it into a loop, kinda struggling with this one

--- game.js
+++ game.js
@@ -10,6 +10,8 @@
     }, 1000);  
     
 }*/
+
+
 const UI = {
     timer: document.querySelector('.timer--value'),
     score: document.querySelector('#score--value'),
@@ -20,52 +22,83 @@ const UI = {
     exitButton: document.querySelector('.exit-button'),
     message: document.querySelector('.status-message'),
     cells: document.querySelectorAll('.cell'),
+
 };
+const enemy = document.getElementById("enemy");
+
+const enemyPosition = {
+    X: parseInt(window.getComputedStyle(enemy).left),
+    Y: parseInt(window.getComputedStyle(enemy).top),
+}
+
+const enemyMovement = { x: 0, y: 0 };
 
 const Game = {
     highscore: null,
     score: 0,
     lives: 3,
     started: false,
+    fjendeposition: null,
+
     updateUI() {
         UI.score.textContent = this.calculateScore().toString();
+    },
+    updatePosition() {
+        let enemy = document.getElementById("enemy");
+        enemy.style.left = enemyPosition.X += 'px';
     },
     calculateScore() {
         return this.highscore;
     },
+    calculatePosition() {
+        return this.fjendeposition;
+    },
 
     start() {
         this.started = true;
+
         this.score =
             setInterval(() => {
                 this.highscore += 1000;
                 this.updateUI();
 
-                if (this.lives <= 0 ) {
+                if (this.lives <= 0) {
                     this.end();
                 }
             }, 1000);
+        this.enemyMovement();
     },
 
     end() {
         this.started = false;
         this.updateUI;
         clearInterval(this.score);
-        
+
+
+    },
+
+    enemyMovement() {
+        if (this.started = true) {
+            setInterval(() => {
+                enemyPosition.X += 100;
+                this.updatePosition();
+
+            }, 1000);
+        }
 
     }
 
-}
+};
 
 //Player constant
 const player = document.getElementById("player");
-const enemy = document.getElementById("enemy");
 const gameContainer = document.getElementById("game-container");
-let position = {
+let positionPlayer = {
     x: parseInt(window.getComputedStyle(player).left),
     y: parseInt(window.getComputedStyle(player).top),
 };
-let movement = {x: 0, y: 0};
+
+let movement = { x: 0, y: 0 };
 
 //Event listener der lytter til hvornår W,A,S,D er trykket ned og retter sig efter det ift. top position og left position i HTML
 window.addEventListener("keydown", function (event) {
@@ -98,12 +131,14 @@ window.addEventListener("keyup", function (event) {
 });
 
 
-const gameLoop = function() {
-    position.x += movement.x;
-    position.y += movement.y;
+const gameLoop = function () {
+    positionPlayer.x += movement.x;
+    positionPlayer.y += movement.y;
 
-    player.style.left = position.x + 'px';
-    player.style.top = position.y + 'px';
+    player.style.left = positionPlayer.x + 'px';
+    player.style.top = positionPlayer.y + 'px';
+
+
 
     window.requestAnimationFrame(gameLoop);
 }
